0312ea7760a799975873698260dc337e6135594c
anjaleena <anjaleenab@github.com>
2019-10-09 11:06:37 -0700

    made superglobal get id be accounted for

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -6,8 +6,6 @@ set_exception_handler('error_handler');
 require_once('db_connection.php');
 
 startup();
-$output = file_get_contents('dummy-products-list.json');
-print $output;
 
 $query = 'SELECT * FROM `Products` WHERE `Price` >2000';
 
