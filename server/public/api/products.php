eecc184a560c80aba807d83bd21d1ca48657018f
anjaleena <anjaleenab@github.com>
2019-10-09 11:34:59 -0700

    accounted for id value > product id value

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,15 +5,28 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once('db_connection.php');
 
+if (!empty($_GET["id"])) {
+  $whereClause = 'WHERE `id` = ' . $_GET["id"];
+} else if(empty($_GET["id"])) {
+  $whereClause = '';
+}
+if(!empty($_GET["id"]) && !is_numeric($_GET["id"])){
+  throw new Exception("Id must be an Integer");
+}
+if($_GET["id"])
+
 startup();
 
-$query = 'SELECT * FROM `Products` WHERE `Price` >2000';
+$query = 'SELECT * FROM `Products`' . $whereClause;
 
 $result= mysqli_query($conn, $query);
 
 $sqlError = mysqli_error($conn);
 if(!$result){
   throw Exception($sqlError);
+}
+if(!mysqli_num_rows($result)){
+  throw new Exception("Invalid ID");
 }
 
 $output = [];
