1d763cd2e5ba18a97df5a6a6c85641c5eeb89f11
anjaleena <anjaleenab@github.com>
2019-10-10 18:31:12 -0700

    added queries and array of images

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -6,17 +6,14 @@ set_exception_handler('error_handler');
 require_once('db_connection.php');
 
 if (!empty($_GET["id"])) {
-  $whereClause = 'SELECT p.ID, p.Name, p.Price, p.ShortDescription, GROUP_CONCAT(i.imageURL) AS URL FROM Products AS p JOIN IMAGES AS i ON p.ID = i.ProductID WHERE p.ID = ' . $_GET["id"] . ' GROUP BY p.ID';
+   $whereClause = "SELECT p.ID, p.Name, p.Price, p.ShortDescription, GROUP_CONCAT(i.imageURL) AS URL FROM Products AS p JOIN IMAGES AS i ON p.ID = i.ProductID WHERE p.ID = " . $_GET["id"] . " GROUP BY p.ID";
 } else if(empty($_GET["id"])) {
-  $whereClause = 'SELECT `ID`, `Name`, `Price`, `ShortDescription`
-FROM `Products` WHERE `ID` = `ProductID`
-LIMIT 1) AS IMAGEURL FROM IMAGES';
+  $whereClause = "SELECT * FROM `Products`";
 }
 
 if(!empty($_GET["id"]) && !is_numeric($_GET["id"])){
   throw new Exception("Id must be an Integer");
 }
-
 
 // SELECT p.ID, p.Name, p.Price, p.ShortDescription, GROUP_CONCAT(i.imageURL) AS URL FROM Products AS p JOIN IMAGES AS i ON p.ID = i.ProductID WHERE p.ID = 1 GROUP BY p.ID
 
@@ -37,8 +34,11 @@ if(!mysqli_num_rows($result)){
 $output = [];
 
 while ($row = mysqli_fetch_assoc($result)){
+  $row["URL"] = explode(',', $row["URL"]);
   $output[] = $row;
-}
+
+  }
+
 $encodedJson = json_encode($output);
 print $encodedJson;
 
