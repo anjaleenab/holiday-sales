0f543c3e5e38736383da1fb6b4c8c97c9143406b
anjaleena <anjaleenab@github.com>
2019-11-25 18:26:28 -0800

    added the cartsummary item component

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import cartSummaryItem from './cart-summary-item';
+import header from './header';
 
 function cartSummary(props) {
-  props.cart.map((cartItem, index) => {
-    return (
-      <cartSummaryItem
-        key={index}
-        image={cartItem.image}
-        name={cartItem.name}
-        price={cartItem.price}
-        shortDescription={cartItem.shortDescription} />
-    );
-  });
+  return (
+    <header />,
+    <div>
+      <div><button>Back to Catalog</button></div>
+      <div>My Cart
+      CART ITEM HERE
+      {props.cart.map((cartItem, index) => {
+        return (
+
+          <cartSummaryItem
+            key={index}
+            image={cartItem.image}
+            name={cartItem.name}
+            price={cartItem.price}
+            shortDescription={cartItem.shortDescription} />
+        );
+      })
+      }
+      </div>
+    </div>
+  );
 }
 
 export default cartSummary;
