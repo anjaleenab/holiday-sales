6f4080300f657273c61589dd5cd3326a7ffa96cc
anjaleena <anjaleenab@github.com>
2019-11-26 17:02:58 -0800

    added values and onChange attributes to inputs on checkout form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -4,8 +4,28 @@ export default class CheckoutForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      nameValue: '',
+      creditCardValue: '',
+      addressValue: ''
     };
-
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCreditCardChange = this.handleCreditCardChange.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+  }
+  handleNameChange(event) {
+    this.setState({
+      nameValue: event.target.value
+    });
+  }
+  handleCreditCardChange(event) {
+    this.setState({
+      creditCardValue: event.target.value
+    });
+  }
+  handleAddressChange(event) {
+    this.setState({
+      addressValue: event.target.value
+    });
   }
   render() {
     return (
@@ -14,11 +34,20 @@ export default class CheckoutForm extends React.Component {
         <div>Order Total: </div>
         <form>
           <label>Name</label>
-          <input placeholder="Name"></input>
+          <input
+            placeholder="Name"
+            value={this.state.nameValue}
+            onChange ={this.handleNameChange}></input>
           <label>Credit Card Number</label>
-          <input placeholder="Credit Card Number"></input>
+          <input
+            placeholder="Credit Card Number"
+            value={this.state.creditCardValue}
+            onChange={this.handleCreditCardChange}></input>
           <label>Shipping Information</label>
-          <input placeholder="Shipping Information"></input>
+          <input
+            placeholder="Shipping Information"
+            value={this.state.addressValue}
+            onChange={this.handleAddressChange}></input>
         </form>
         <div>
           <button
@@ -26,7 +55,8 @@ export default class CheckoutForm extends React.Component {
             onClick={() => this.props.viewSetter('catalog', null)}>Continue Shopping</button>
           <button
             className="btn"
-            onClick={() => this.props.viewSetter('catalog', null)}>Place Order</button>
+            // onClick=
+          >Place Order</button>
         </div>
       </div>
     );
