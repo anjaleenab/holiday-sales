c725bb2a7a9680ce0c4a2b5144a5615197ada5e8
anjaleena <anjaleenab@github.com>
2019-11-30 19:53:37 -0800

    styled checkout form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -35,40 +35,45 @@ export default class CheckoutForm extends React.Component {
   }
   render() {
     return (
-      <div>
-        <div className="column">Checkout
+      <div className="container m-3">
+        <div>Checkout
           <div>Order Total: </div>
-          <form>
-            <div className="form-group">
-              <label>Name</label>
-              <input
-                placeholder="Name"
-                value={this.state.nameValue}
-                onChange ={this.handleNameChange}></input>
-            </div>
-            <div className="form-group">
-              <label>Credit Card Number</label>
-              <input
-                placeholder="Credit Card Number"
-                value={this.state.creditCardValue}
-                onChange={this.handleCreditCardChange}></input>
-            </div>
-            <div className="form-group">
-              <label>Shipping Information</label>
-              <input
-                placeholder="Shipping Information"
-                value={this.state.addressValue}
-                onChange={this.handleAddressChange}></input>
+          <form className="ml-5 mt-2">
+            <div className="m-5" >
+              <div className="form-group row">
+                <label className="col-md-4">Name</label>
+                <input
+                  className="col-md-8"
+                  placeholder="Name"
+                  value={this.state.nameValue}
+                  onChange={this.handleNameChange}/>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-4">Credit Card Number</label>
+                <input
+                  className="col-md-8"
+                  placeholder="Credit Card Number"
+                  value={this.state.creditCardValue}
+                  onChange={this.handleCreditCardChange}/>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-4">Shipping Information</label>
+                <input
+                  className="col-md-8"
+                  placeholder="Shipping Information"
+                  value={this.state.addressValue}
+                  onChange={this.handleAddressChange}/>
+              </div>
             </div>
           </form>
         </div>
-        <div>
+        <div className="checkoutFormButtons">
           <button
-            className="btn btn-dark"
+            className="btn btn-dark contShop"
             onClick={() => this.props.viewSetter('catalog', null)}
           >Continue Shopping</button>
           <button
-            className="btn btn-dark"
+            className="btn btn-dark float-right mr-1"
             onClick={this.handleSubmit}
           >Place Order</button>
         </div>
