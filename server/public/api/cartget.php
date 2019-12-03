f312f847547614cb12e60968aee6fa7a635509a1
anjaleena <anjaleenab@github.com>
2019-12-02 16:57:10 -0800

    sent query for cart to the database and retrieved the data and sent it to the front endeeeeeeeeeeee

--- server/public/api/cartget.php
+++ server/public/api/cartget.php
@@ -13,10 +13,22 @@ $cartId = intval(SESSION['cart_id']);
 
 //can use cartid here for order confirmation or make a separate query for it later
 $cartInfoQuery= "SELECT cartItems.`id`,cartItems.`productID`,cartItems.`count`,cartItems.`price`,
-Products.`name`, Products.`ShortDescription`, Products.`Image`
-FROM cartItems
-INNER JOIN Products
-ON cartItems.productID = Products.ID
-WHERE cartItems.cartID= $cartId";
+                Products.`name`, Products.`ShortDescription`, Products.`Image`
+                FROM cartItems
+                INNER JOIN Products
+                ON cartItems.productID = Products.ID
+                WHERE cartItems.cartID= $cartId";
+
+$result = mysqli_query($conn, $cartInfoQuery);
+
+$cartInfo = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $row["orderNumber"] = intval($row["id"]);
+  $row['productID'] =intval($row['productID']);
+  $row['price']=intval($row['price']);
+  $cartInfo = $row;
+}
+
+print_r(json.encoded($cartInfo));
 
 ?>
