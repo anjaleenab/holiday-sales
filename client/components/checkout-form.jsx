f0967820e8497e183b5ba9468802b1b1d4a112b8
anjaleena <anjaleenab@github.com>
2019-12-10 16:24:58 -0800

    added col-xl to the form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -39,9 +39,7 @@ export default class CheckoutForm extends React.Component {
   render() {
     var orderAmount = this.props.getOrderAmount();
     return (
-
       <div className="container col-xl-10 m-3">
-
         <div className="checkout">Checkout
           <div>Order Total: {orderAmount} </div>
           <form className="ml-5 mt-2">
@@ -50,10 +48,9 @@ export default class CheckoutForm extends React.Component {
                 <label className="col-md-4">Name</label>
                 <input
                   className="col-md-8 input"
-
                   placeholder="Name"
                   value={this.state.nameValue}
-                  onChange={this.handleNameChange}/>
+                  onChange={this.handleNameChange} />
               </div>
               <div className="form-group row">
                 <label className="col-md-4">Credit Card Number</label>
@@ -61,7 +58,7 @@ export default class CheckoutForm extends React.Component {
                   className="col-md-8 input"
                   placeholder="Credit Card Number"
                   value={this.state.creditCardValue}
-                  onChange={this.handleCreditCardChange}/>
+                  onChange={this.handleCreditCardChange} />
               </div>
               <div className="form-group row">
                 <label className="col-md-4">Shipping Information</label>
@@ -69,7 +66,7 @@ export default class CheckoutForm extends React.Component {
                   className="col-md-8 input"
                   placeholder="Shipping Information"
                   value={this.state.addressValue}
-                  onChange={this.handleAddressChange}/>
+                  onChange={this.handleAddressChange} />
               </div>
             </div>
           </form>
