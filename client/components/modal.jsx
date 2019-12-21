5b5b1cb8d1e7589fac1084cad57d2a40ca929025
anjaleena <anjaleenab@github.com>
2019-12-20 18:54:27 -0800

    styled the disclaimer modal

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 function Modal(props) {
   var seenDisclaimer = true;
   var component = <div className="disclaimer">
-  DISCLAIMER: This application is for demonstration purposes only.None of the items on this website are actually
-    for sale at this location.No transactions will be processed.Please do not enter any personal information
+  DISCLAIMER: This application is for demonstration purposes only. None of the items on this website are actually
+    for sale at this location. No transactions will be processed. Please do not enter any personal information
     while using the website.
     <button
+      className= "btn disclaim"
       onClick={() => {
         seenDisclaimer = false;
       }
