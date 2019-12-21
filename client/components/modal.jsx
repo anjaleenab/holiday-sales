921f7354398bed9ad6bf64365a11a1e1541bec79
anjaleena <anjaleenab@github.com>
2019-12-20 19:27:12 -0800

    added functionality to the modal

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 
 function Modal(props) {
-  var seenDisclaimer = true;
   var component = <div className="disclaimer">
   DISCLAIMER: This application is for demonstration purposes only. None of the items on this website are actually
     for sale at this location. No transactions will be processed. Please do not enter any personal information
     while using the website.
     <button
       className= "btn disclaim"
-      onClick={() => {
-        seenDisclaimer = false;
-      }
-      }> By clicking this button, I acknowledge the above statement
+      onClick={() => props.verifyDisclaimer()}> By clicking this button, I acknowledge the above statement
     </button>
   </div>;
-  if (seenDisclaimer) {
-    return (
-      component
-    );
-  } else {
-    return (
-      null
-    );
-  }
+  return (
+    component
+  );
 }
 export default Modal;
