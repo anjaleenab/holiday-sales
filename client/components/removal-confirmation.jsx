2e93ffd98599097fbfc316f4528837210fdbdbda
anjaleena <anjaleenab@github.com>
2020-01-10 16:03:36 -0800

    added the do not remove button and a div for both of the removal confirmation buttons

--- client/components/removal-confirmation.jsx
+++ client/components/removal-confirmation.jsx
@@ -4,10 +4,15 @@ function RemovalConf(props) {
   if (props.productID === props.removal.productID) {
     return (
       <div className="removalConf"> Are you sure you would like to remove each {props.productName} from your cart?
-        <button
-          className="btn btn-dark finalRemoval"
-          onClick={() => props.removeItem(props.productID, props.cartID)}>Remove
-        </button>
+        <div className="removalButtons">
+          <button
+            className="btn btn-dark finalRemoval"
+            onClick={() => props.removeItem(props.productID, props.cartID)}>Remove
+          </button>
+          <button
+            className="btn btn-dark declineRemoval"
+            onClick={() => props.removeModal(false)}> Do Not Remove </button>
+        </div>
       </div>
     );
   } else {
